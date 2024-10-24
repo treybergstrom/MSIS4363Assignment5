26e3cf44bc96cf9211d490e96f16af939598edda
Trey Bergstrom <trey.bergstrom@okstate.edu>
2024-10-24 12:39:27 -0500

    Add project files.

---
+++ MSIS4363Assignment5/Pages/Privacy.cshtml.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MSIS4363Assignment5.Pages
+{
+	public class PrivacyModel : PageModel
+	{
+		private readonly ILogger<PrivacyModel> _logger;
+
+		public PrivacyModel(ILogger<PrivacyModel> logger)
+		{
+			_logger = logger;
+		}
+
+		public void OnGet()
+		{
+		}
+	}
+
+}
